2e849e34e1384c6ebc5d8defb39efd21e1e8aa0d
nathan <nathan.consolo17@gmail.com>
2025-05-16 23:41:15 -0300

    feat: courtyard screen

--- src/components/Setores.jsx
+++ src/components/Setores.jsx
@@ -1,12 +1,51 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function Setores() {
+export default function Setores({ nome, icone, cor }) {
   return (
-    <View>
-      <Text>Setores</Text>
-    </View>
+    <TouchableOpacity style={styles.container}>
+      <Text style={styles.nome}>{nome}</Text>
+      <Ionicons name={icone} size={30} color="#d1d9e6" marginHorizontal={30} />
+      <View style={[styles.barraCor, { backgroundColor: cor }]} />
+    </TouchableOpacity>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#1a2639",
+    borderRadius: 16,
+    paddingVertical: 16,
+    paddingHorizontal: 20,
+    marginVertical: 12,
+    marginHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    shadowColor: "#ffffff",
+    shadowOffset: { width: 10, height: 40 },
+    shadowOpacity: 1,
+    shadowRadius: 80,
+    elevation: 4,
+    borderWidth: 1,
+    borderColor: "#d1d9e6",
+    position: "relative",
+    marginVertical: "4%",
+  },
+  nome: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#d1d9e6",
+    flex: 1,
+  },
+  barraCor: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    width: "5%",
+    height: "202%",
+    borderTopRightRadius: 16,
+    borderBottomRightRadius: 16,
+  },
+});
--- src/pages/Patio.jsx
+++ src/pages/Patio.jsx
@@ -1,11 +1,54 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import Setores from "../components/Setores";
 
 export default function Patio() {
   return (
-    <View>
-      <Text>Patio</Text>
+    <View style={styles.container}>
+      <Text style={styles.sectionTitle}>Patio</Text>
+      <Text style={styles.info}>Selecione o setor que deseja verificar</Text>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <Setores
+          nome="Pronta para aluguel"
+          icone="thumbs-up-sharp"
+          cor="green"
+        />
+        <Setores nome="Pendente" icone="time-sharp" cor="yellow" />
+        <Setores nome="Sem placa" icone="close" cor="white" />
+        <Setores nome="Danos estruturais graves" icone="warning" cor="red" />
+        <Setores nome="Reparo simples" icone="construct" cor="blue" />
+        <Setores nome="Agendada para manutenção" icone="clipboard" cor="grey" />
+        <Setores nome="Motor defeituoso" icone="cog" cor="orange" />
+        <Setores nome="Minha Mottu" icone="person" cor="#00C853" />
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1a2639",
+    paddingTop: "12%",
+    paddingBottom: "15%",
+  },
+  sectionTitle: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 15,
+    alignSelf: "center",
+  },
+  info: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "400",
+    marginBottom: 15,
+    alignSelf: "center",
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingBottom: 20,
+  },
+});
