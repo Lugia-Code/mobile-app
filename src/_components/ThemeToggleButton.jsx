992b67063bc355bbbb5039f3e50bbdebf3847546
nathan <nathan.consolo17@gmail.com>
2025-09-04 23:52:35 -0300

    style(theme button): size adjusted

--- src/_components/ThemeToggleButton.jsx
+++ src/_components/ThemeToggleButton.jsx
@@ -1,6 +1,5 @@
 import { TouchableOpacity, StyleSheet } from "react-native";
 import { useTheme } from "../context/ThemeContext";
-import { Image } from "expo-image";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function ThemeToggleButton() {
@@ -13,7 +12,7 @@ export default function ThemeToggleButton() {
     >
       <Ionicons
         name={theme == "light" ? "sunny" : "moon"}
-        size={32}
+        size={34}
         color="#000"
       />
     </TouchableOpacity>
@@ -22,9 +21,12 @@ export default function ThemeToggleButton() {
 
 const styles = StyleSheet.create({
   button: {
-    paddingVertical: 12,
-    paddingHorizontal: 24,
-    borderRadius: 8,
+    padding: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    width: 50,
+    height: 50,
+    borderRadius: 100,
     marginTop: 20,
   },
   text: {
