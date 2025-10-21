c87179a8185b13a80aeec1ed1bbcb4ae4df09638
nathan <nathan.consolo17@gmail.com>
2025-10-21 18:41:01 -0300

    style: tag's screen stylized

--- src/pages/Tag/index.jsx
+++ src/pages/Tag/index.jsx
@@ -1,11 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Cabecalho from "../../_components/Cabecalho";
+import { logOut } from "../../utils/navigation";
+import ContainerScreens from "../../_components/ContainerScreens";
 
-export default function Tag() {
+export default function Tag({ navigation }) {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <>
+      <Cabecalho
+        iconName="logout"
+        onIconPress={() => logOut(navigation)}
+        title="Informações das Tags"
+      />
+      <ContainerScreens></ContainerScreens>
+    </>
   );
 }
 
